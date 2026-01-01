aa43daad9236cb2d1c7fd060307ba065876cd296
HamaisT <hamaistanvir@gmail.com>
2026-01-01 15:33:23 +0500

    controllers modification

--- backend/src/Controllers/notesControllers.js
+++ backend/src/Controllers/notesControllers.js
@@ -17,9 +17,10 @@ export async function getAllNotes(req, res){
 export async function createNote(req, res) {
       try {
        const { title, content } = req.body;
-       const newNote = new Note({title, content});
-       await newNote.save();
-       res.status(201).json({messsage: 'Note created successfully'})
+       const note = new Note({title, content});
+
+       const savedNote = await note.save();
+       res.status(201).json(savedNote)
       } catch (error) {
         console.error('Error in createNote controller:', error);
         res.status(500).json({message:'Internal server error'});
@@ -27,7 +28,17 @@ export async function createNote(req, res) {
 }
 
 export async function updateNote(req, res) {
-    res.status(200).json({ message: `Note with id ${req.params.id} updated successfully` });
+    try {
+        const { title, content } = req.body;
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id, {title, content}, {new:true});
+
+        if(!updatedNote) return res.status(404).json({message: 'Note not found'});
+        res.status(200).json({message:`Note with id ${req.params.id} updated successfully`, updatedNote});
+    
+    } catch (error) {
+        console.error('Error in updateNote controller:', error);
+        res.status(500).json({message:'Internal server error'});
+    }
 }
 
 export async function deleteNote(req, res) { 
